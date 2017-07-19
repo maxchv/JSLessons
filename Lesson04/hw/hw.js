94c625babdc6335a8c6c028dc7d7c257046c3d54
Maxim <maxshaptala@gmail.com>
2017-07-19 21:46:23 +0300

    добавлена домашка по dom

---
+++ Lesson04/hw/hw.js
@@ -0,0 +1,19 @@
+function isPalindrom(str) {
+    return str;
+}
+
+function ucFirst(str) {
+    return str;
+}
+
+function checkSpam(str) {
+    return str;
+}
+
+function extractCurrencyValue(str) {
+    return '120';
+}
+
+function toAlphaBet(str) {
+    return str;
+}
---
+++ Lesson04/hw/tests.js
@@ -0,0 +1,25 @@
+QUnit.test( "Задание 1. Написать функцию isPalindrom(str)", function( assert ) {
+	assert.equal(isPalindrom('madam'), true , "Это должен быть палиндром!" );
+	assert.equal(isPalindrom('mama'), true , "Это не палиндром!" );
+});
+
+QUnit.test( "Задание 2. Напишите функцию ucFirst(str)", function( assert ) {
+	assert.equal(ucFirst('anna'), 'Anna' , "Failed!" );
+	assert.equal(ucFirst('java'), 'Java' , "Failed!" );
+	assert.equal(ucFirst('hello world'), 'Hello World'  , "Failed!" );
+});
+
+QUnit.test( "Задание 3. Напишите функцию checkSpam(str)", function( assert ) {
+	assert.equal(checkSpam('buy ViAgRA now'), true , "Это спам!" );
+	assert.equal(checkSpam('free xxxxx'), true , "Это спам!" );
+	assert.equal(checkSpam("innocent rabbit"), false, "Это не спам!" );
+});
+
+QUnit.test( "Задание 4.  Создайте функцию extractCurrencyValue(str)", function( assert ) {
+	assert.deepEqual(extractCurrencyValue('$120'), 120, "Это не число!" );
+});
+
+QUnit.test( "Задание 5. Напишите функцию toAlphaBet(str)", function( assert ) {
+	assert.equal(toAlphaBet('webmaster'), 'abeemrstw' , "Это не алфавитный порядок!" );
+
+});
